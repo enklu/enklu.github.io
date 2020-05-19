4bf3502cc1f2a9114d3907377ddb7617a8c2e92a
Shannon <shannonkalei@gmail.com>
2020-05-18 20:02:22 -0700

    Deploy website
    Deploy website version based on 8b2ce4fef4af408b49ee1612e003b956fcc2a4f1

--- js/code-block-buttons.js
+++ js/code-block-buttons.js
@@ -1,5 +1,4 @@
-// Turn off ESLint for this file because it's sent down to users as-is.
-/* eslint-disable */
+
 window.addEventListener('load', function() {
   function button(label, ariaLabel, icon, className) {
     const btn = document.createElement('button');
